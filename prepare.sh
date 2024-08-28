92bd4755bc2a5773343cff00edb0daac8bb01141
wansyu <12646240+wansyu@users.noreply.github.com>
2024-08-29 02:37:09 +0800

    feat: Update prepare.sh to dynamically set the webservice port
    This commit modifies the prepare.sh script to dynamically set the webservice port in the file "root/etc/s6-overlay/s6-rc.d/svc-duplicati/run". It replaces the hardcoded port number 8200 with the value of the WEBUI environment variable. Additionally, it adds a new option "--webservice-port=${WEBUI}" to the same file. These changes improve the flexibility and configurability of the Docker container.

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,12 @@
 mkdir -p root
 curl -L https://github.com/linuxserver/docker-duplicati/tarball/master | tar xz --strip=1 --wildcards "*/root/*" -C root
 # replace 8200 in file "root/etc/s6-overlay/s6-rc.d/svc-duplicati/run"  with "${WEBUI}"
-sed -i 's/8200/${WEBUI}/g' root/etc/s6-overlay/s6-rc.d/svc-duplicati/run
+TARGET_FILE="root/etc/s6-overlay/s6-rc.d/svc-duplicati/run"
+sed -i 's/8200/${WEBUI}/g' "$TARGET_FILE"
+# add a option " --webservice-port=${WEBUI}" to the file "root/etc/s6-overlay/s6-rc.d/svc-duplicati/run" at last
+OPTION=" --webservice-port=\${WEBUI}"
+sed -i "\$s/\$/${OPTION}/" "$TARGET_FILE"
+
 # Download the "Dockerfile"
 curl -L https://raw.githubusercontent.com/linuxserver/docker-duplicati/master/Dockerfile -o Dockerfile
 # add a line "ENV WEBUI=8200" to the file "Dockerfile" after the line "ENV HOME="/config""
